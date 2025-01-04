ed32d743b51523a6be85e40b9f2fa1255e63b3e6
dhson2 <hungsondo1999@gmail.com>
2025-01-04 21:23:43 +0700

    Set up router view

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+
+
+const routes = [
+    {
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/about',
+        component: About
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+createApp(App).use(router).mount('#app')
